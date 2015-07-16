8f5a2f98d4b40072163e04f6b9221bbb98201342
lifuz <lifuzz@163.com>
2015-07-16 14:14:00 +0800

    初步成功

--- app/src/main/java/com/prd/yzy/gj/EmployeeActivity.java
+++ app/src/main/java/com/prd/yzy/gj/EmployeeActivity.java
@@ -1,10 +1,17 @@
 package com.prd.yzy.gj;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.inqbarna.tablefixheaders.TableFixHeaders;
+import com.inqbarna.tablefixheaders.adapters.BaseTableAdapter;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -33,6 +40,8 @@ public class EmployeeActivity extends BaseActivity  {
     private AsyncHttpClient client;
     private RequestParams params;
 
+    private TableFixHeaders tfh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +59,8 @@ public class EmployeeActivity extends BaseActivity  {
 
         client = new AsyncHttpClient();
         params = new RequestParams();
+
+        tfh = (TableFixHeaders) findViewById(R.id.yg_table);
 
         String sgid = share.getString("suid","");
 
@@ -86,9 +97,217 @@ public class EmployeeActivity extends BaseActivity  {
                     }
                 }
 
-                Log.i("tag",customers.toString());
+//                Log.i("tag",customers.toString());
+                tfh.setAdapter(new MyAdapter(getApplicationContext()));
 
             }
         });
+    }
+
+
+    public class MyAdapter extends BaseTableAdapter {
+
+        private final Context context;
+        private final LayoutInflater inflater;
+
+        private final int width;
+        private final int height;
+
+        public MyAdapter(Context context) {
+            super();
+            this.context = context;
+            inflater = LayoutInflater.from(context);
+
+            //从配置文件获取高度和宽度
+            Resources rs = context.getResources();
+
+            width = rs.getDimensionPixelSize(R.dimen.table_width);
+            height = rs.getDimensionPixelSize(R.dimen.table_height);
+
+
+        }
+
+        public Context getContext() {
+            return context;
+        }
+
+        public LayoutInflater getInflater() {
+            return inflater;
+        }
+
+
+        /**
+         * 设置行数
+         *
+         * @return
+         */
+        @Override
+        public int getRowCount() {
+            return customers.size();
+        }
+
+        /**
+         * 设置列数
+         *
+         * @return
+         */
+        @Override
+        public int getColumnCount() {
+            return 4;
+        }
+
+        /**
+         * 为每个View设置布局和值
+         *
+         * @param row       The row of the item within the adapter's data table of the
+         *                  item whose view we want. If the row is <code>-1</code> it is
+         *                  the header.
+         * @param column    The column of the item within the adapter's data table of the
+         *                  item whose view we want. If the column is <code>-1</code> it
+         *                  is the header.
+         * @param view
+         * @param viewGroup
+         * @return
+         */
+        @Override
+        public View getView(final int row, final int column, View view, ViewGroup viewGroup) {
+
+            if (view == null) {
+                view = inflater.inflate(getLayoutResource(row, column), viewGroup, false);
+            }
+
+            ((TextView) view.findViewById(R.id.item_tv)).setText(getCellString(row, column));
+
+
+            if (row >= 0) {
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        Toast.makeText(MainActivity.this, getCellString(row, column), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+
+            return view;
+        }
+
+        /**
+         * 设置每个View的值
+         *
+         * @param row
+         * @param column
+         * @return
+         */
+
+        public String getCellString(int row, int column) {
+
+            if (row <0) {
+                switch (column){
+                    case -1:
+                        return "cid";
+                    case 0:
+                        return "姓名";
+                    case 1:
+                        return "手机号";
+                    case 2:
+                        return "性别";
+                    case 3:
+                        return "驾驶号";
+                }
+
+            } else {
+                switch (column) {
+                    case -1:
+                        return customers.get(row).getCid();
+                    case 0:
+                        return customers.get(row).getCname();
+                    case 1:
+                        return customers.get(row).getCphone();
+                    case 2:
+                        return customers.get(row).getSex();
+                    case 3:
+                        return customers.get(row).getDriverOperId();
+                }
+            }
+
+            return "Lorem (" + row + ", " + column + ")";
+        }
+
+        /**
+         * 设置每个View的宽度
+         *
+         * @param i
+         * @return
+         */
+        @Override
+        public int getWidth(int i) {
+            return width;
+        }
+
+        /**
+         * 设置每个View的高度
+         *
+         * @param i
+         * @return
+         */
+        @Override
+        public int getHeight(int i) {
+            return height;
+        }
+
+        /**
+         * 根据行数确定view的类型
+         *
+         * @param row    The row of the item within the adapter's data table of the
+         *               item whose view we want. If the row is <code>-1</code> it is
+         *               the header.
+         * @param column The column of the item within the adapter's data table of the
+         *               item whose view we want. If the column is <code>-1</code> it
+         *               is the header.
+         * @return
+         */
+        @Override
+        public int getItemViewType(int row, int column) {
+            if (row < 0) {
+                return 0;
+            } else {
+                return 1;
+            }
+
+        }
+
+        /**
+         * 根据view的类型选择相应的布局
+         *
+         * @param row
+         * @param column
+         * @return
+         */
+        public int getLayoutResource(int row, int column) {
+            final int layoutResource;
+            switch (getItemViewType(row, column)) {
+                case 0:
+                    layoutResource = R.layout.table_header_item;
+                    break;
+                case 1:
+                    layoutResource = R.layout.table_item;
+                    break;
+                default:
+                    throw new RuntimeException("wtf?");
+            }
+            return layoutResource;
+        }
+
+
+        /**
+         * 设置View的类型总数
+         *
+         * @return
+         */
+        @Override
+        public int getViewTypeCount() {
+            return 2;
+        }
     }
 }
