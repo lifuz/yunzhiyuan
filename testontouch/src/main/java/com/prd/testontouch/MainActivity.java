b32c931e68001d6dda30d962e2bed40e83ac0dab
lifuz <lifuzz@163.com>
2015-07-17 09:48:07 +0800

    测试完成，达成目的

--- testontouch/src/main/java/com/prd/testontouch/MainActivity.java
+++ testontouch/src/main/java/com/prd/testontouch/MainActivity.java
@@ -2,8 +2,11 @@ package com.prd.testontouch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +30,34 @@ public class MainActivity extends Activity {
             list.add("item"+i);
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2,list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,list);
 
         lv.setAdapter(adapter);
+
+        lv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_UP:
+
+                        Toast.makeText(MainActivity.this,"MotionEvent.ACTION_UP",Toast.LENGTH_SHORT).show();
+
+                        break;
+
+                    case MotionEvent.ACTION_DOWN:
+                        Toast.makeText(MainActivity.this,"MotionEvent.ACTION_DOWN",Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        Toast.makeText(MainActivity.this,"MotionEvent.ACTION_MOVE",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
+
     }
 
 
