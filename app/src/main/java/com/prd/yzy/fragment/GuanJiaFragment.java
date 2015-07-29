4dc848b79047a2e43a8134e009da3dc8bedf4356
lifuz <lifuzz@163.com>
2015-07-29 16:25:12 +0800

    库存界面完成

--- app/src/main/java/com/prd/yzy/fragment/GuanJiaFragment.java
+++ app/src/main/java/com/prd/yzy/fragment/GuanJiaFragment.java
@@ -12,6 +12,7 @@ import com.prd.yzy.R;
 import com.prd.yzy.gj.BOMActivity;
 import com.prd.yzy.gj.CaiWuActivity;
 import com.prd.yzy.gj.EmployeeActivity;
+import com.prd.yzy.gj.KuCunActivity;
 import com.prd.yzy.gj.SheBeiActivity;
 import com.prd.yzy.gj.ShengChanActivity;
 import com.prd.yzy.gj.XiaoshouActivity;
@@ -117,7 +118,7 @@ public class GuanJiaFragment extends Fragment implements View.OnClickListener {
 
             case R.id.gj_kc:
 
-                startActivity(new Intent(getActivity(), EmployeeActivity.class));
+                startActivity(new Intent(getActivity(), KuCunActivity.class));
                 break;
 
             case R.id.gj_cg:
