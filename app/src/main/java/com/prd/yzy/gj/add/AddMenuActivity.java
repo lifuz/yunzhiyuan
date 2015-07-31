d027056a1be3ec2515e29e877c6c517b1bc70c76
lifuz <lifuzz@163.com>
2015-07-31 14:12:43 +0800

    今天完成的第一个界面

---
+++ app/src/main/java/com/prd/yzy/gj/add/AddMenuActivity.java
@@ -0,0 +1,20 @@
+package com.prd.yzy.gj.add;
+
+import android.os.Bundle;
+
+import com.prd.yzy.BaseActivity;
+import com.prd.yzy.R;
+
+/**
+ * Created by 李富 on 2015/7/31.
+ */
+public class AddMenuActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.addmenu_layout);
+
+    }
+}
