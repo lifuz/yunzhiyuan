2a68dfff45cd45be459e07d86fdb762f379761e2
lifuz <lifuzz@163.com>
2015-08-03 09:59:18 +0800

    积分界面完成

--- app/src/main/java/com/prd/yzy/fragment/WoFragment.java
+++ app/src/main/java/com/prd/yzy/fragment/WoFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 
 import com.prd.yzy.LoginActivity;
 import com.prd.yzy.R;
+import com.prd.yzy.wo.JFAcitivity;
 
 /**
  * Created by 李富 on 2015/7/7.
@@ -21,6 +22,8 @@ public class WoFragment extends Fragment implements View.OnClickListener{
 
     SharedPreferences shared;
     SharedPreferences.Editor editor;
+
+    private View wo_jfgl;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,6 +37,9 @@ public class WoFragment extends Fragment implements View.OnClickListener{
 
         wo_shezhi = view.findViewById(R.id.wo_shezhi);
         wo_shezhi.setOnClickListener(this);
+
+        wo_jfgl = view.findViewById(R.id.wo_jfgl);
+        wo_jfgl.setOnClickListener(this);
 
         shared = getActivity().getSharedPreferences("login", Activity.MODE_PRIVATE);
         editor = shared.edit();
@@ -54,6 +60,10 @@ public class WoFragment extends Fragment implements View.OnClickListener{
                 startActivity(new Intent(getActivity(), LoginActivity.class));
 
                 break;
+
+            case R.id.wo_jfgl:
+
+                startActivity(new Intent(getActivity(), JFAcitivity.class));
         }
 
     }
--- app/src/main/java/com/prd/yzy/wo/JFAcitivity.java
+++ app/src/main/java/com/prd/yzy/wo/JFAcitivity.java
@@ -1,7 +1,90 @@
 package com.prd.yzy.wo;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.prd.yzy.BaseActivity;
+import com.prd.yzy.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by 李富 on 2015/8/3.
  */
-public class JFAcitivity {
+public class JFAcitivity extends BaseActivity implements View.OnClickListener{
+
+    private TextView jf_dq;
+
+    private View jfgl_back;
+
+    private List<Map<String,String>> list;
+
+    private ListView jf_list;
+    private Map<String,String> map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jifen_layout);
+
+        initViews();
+
+    }
+
+    public void initViews(){
+
+        jf_dq = (TextView) findViewById(R.id.jf_dq);
+        jf_dq.setText("当前积分:" + 20);
+
+        jfgl_back = findViewById(R.id.jfgl_back);
+        jfgl_back.setOnClickListener(this);
+
+        jf_list = (ListView) findViewById(R.id.jf_list);
+
+        list = new ArrayList<>();
+        for (int i = 0;i < 5 ;i ++){
+            map = new HashMap<>();
+            map.put("date","2015/8/" +(5-i));
+            map.put("event","分享新闻");
+            map.put("jf",2+"");
+            list.add(map);
+
+            map = new HashMap<>();
+            map.put("date","2015/8/" +(5-i));
+            map.put("event","分享活动");
+            map.put("jf",2+"");
+            list.add(map);
+
+
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(this,list,R.layout.jf_item,
+                new String[]{"date","event","jf"},
+                new int[] {R.id.jf_date,R.id.jf_event,R.id.jf_jf});
+
+        jf_list.setAdapter(adapter);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.jfgl_back:
+
+                finish();
+
+                break;
+        }
+
+    }
 }
