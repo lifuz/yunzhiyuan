107b2ec1a11d12efc5b5d61acd04f3873dfec347
lifuz <lifuzz@163.com>
2015-08-04 10:58:29 +0800

    大部分界面都已完成

---
+++ app/src/main/java/com/prd/yzy/gj/add/AddCGActivity.java
@@ -0,0 +1,57 @@
+package com.prd.yzy.gj.add;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.prd.yzy.BaseActivity;
+import com.prd.yzy.R;
+
+/**
+ * Created by 李富 on 2015/7/31.
+ */
+public class AddCGActivity extends BaseActivity implements View.OnClickListener {
+
+
+    private View menu_back;
+    private TextView menu_back_title,menu_title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.addmenu_layout);
+
+        initViews();
+
+    }
+
+    public void initViews(){
+
+        menu_back = findViewById(R.id.menu_back);
+        menu_back.setOnClickListener(this);
+
+        menu_back_title = (TextView) findViewById(R.id.menu_back_title);
+        menu_back_title.setText(" 采购");
+
+        menu_title = (TextView) findViewById(R.id.menu_title);
+        menu_title.setText("添加采购");
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.menu_back:
+
+                finish();
+
+                break;
+
+        }
+    }
+}
