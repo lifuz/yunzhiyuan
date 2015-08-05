b7e5216e8b77243cb9b99a17bda9e3067303c2c6
lifuz <lifuzz@163.com>
2015-08-05 10:43:36 +0800

    修改spinner的样式

--- app/src/main/java/com/prd/yzy/gj/add/AddMenuActivity.java
+++ app/src/main/java/com/prd/yzy/gj/add/AddMenuActivity.java
@@ -2,6 +2,8 @@ package com.prd.yzy.gj.add;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.prd.yzy.BaseActivity;
 import com.prd.yzy.R;
@@ -13,6 +15,8 @@ public class AddMenuActivity extends BaseActivity implements View.OnClickListene
 
 
     private View menu_back;
+
+    private Spinner spinner_cp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,14 @@ public class AddMenuActivity extends BaseActivity implements View.OnClickListene
 
         menu_back = findViewById(R.id.menu_back);
         menu_back.setOnClickListener(this);
+
+        spinner_cp = (Spinner) findViewById(R.id.spinner_cp);
+
+        String[] arr = { "空气阀","开关阀","电磁阀","平衡车" };
+        //设置spinner的样式和下拉的值
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,arr);
+
+        spinner_cp.setAdapter(adapter);
 
 
 
